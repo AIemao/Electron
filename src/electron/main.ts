1f130d0150ce8b51fc192c2bc9683ed77daf6a40
Celio Junior <celioju@hotmail.com>
2025-04-27 11:52:17 -0300

    Adicionado Dev Tools

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,23 @@
 import { app, BrowserWindow } from 'electron';
 import path from 'path';
 import { isDev } from './utils.js';
+import { installExtension, REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
 
 let mainWindow: BrowserWindow | null = null;
 
+// Verifica se o app já está rodando (evita abrir mais de uma instância):
+const isRunning = app.requestSingleInstanceLock();
+if (!isRunning) {
+  app.quit(); // Exit if this is a second instance
+} else {
+  // Handle second instance attempts
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
+  });
+}
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1024,
@@ -18,6 +32,14 @@ function createWindow() {
   });
 
   if (isDev()) {
+    // Instala o React DevTools no modo de desenvolvimento:
+    installExtension(REACT_DEVELOPER_TOOLS)
+    .then((extension) =>
+      console.log(`Extensão adicionada: ${extension.name || extension}`)
+    )
+    .catch((err: Error) =>
+      console.error('Erro ao adicionar extensão:', err)
+    ); 
     mainWindow.loadURL('http://localhost:5123');
     mainWindow.webContents.openDevTools({ mode: 'detach' });
   } else {
